7167c3ad2ff45f1a7a374b1eaf439102537e7d5e
darionz <bairovpaul@gmail.com>
2025-02-27 17:10:15 +0700

    Auto tests

---
+++ tests/moneyTest.js
@@ -0,0 +1,22 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('test formatCurrency');
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('zero case');
+if (formatCurrency(0) === '0.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('rounding case')
+if (formatCurrency(2000.4) === '20.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}

6ed70ee4bf7d879d0ce38fa19e86011f66f22feb
darionz <bairovpaul@gmail.com>
2025-03-04 14:40:03 +0700

    Load products from backend

--- data/products.js
+++ data/products.js
@@ -54,7 +54,7 @@ class Clothing extends Product {
 }
 
 
-export const products = [
+/*export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -719,3 +719,23 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
+
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr  = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log(products);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}

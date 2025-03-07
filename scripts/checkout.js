dd51caa4268ecd19d542d0676973e32ca0bfa4db
darionz <bairovpaul@gmail.com>
2025-03-07 11:16:54 +0700

    Convert xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,14 +1,30 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 
+
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) =>{
+    loadCart(() => {
+      resolve();
+    })
+  })
+]).then((values) => {
+  console.log(values);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 new Promise((resolve) => {
-  loadProducts(() => {
+  loadProductsFetch(() => {
     resolve();
   });
 }).then(() => {
   renderOrderSummary()
   renderPaymentSummary()
 });
-
+*/
